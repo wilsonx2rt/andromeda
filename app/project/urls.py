9cf6957dca1ba12d78b9b10676b35f8f9d937d58
Stewart Wilson <wilson.x2rt@gmail.com>
2018-04-30 10:34:29 +0200

    URLs setup typo

--- app/project/urls.py
+++ app/project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('projects.api.url', namespace='api')),
+    path('api/', include('project.api.urls', namespace='api')),
 ]
