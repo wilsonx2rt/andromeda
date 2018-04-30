edb266e66d73de4e69f188b5296c3c47f901cd55
Stewart Wilson <wilson.x2rt@gmail.com>
2018-04-30 12:58:09 +0200

    Auth enpoints

--- app/project/api/urls.py
+++ app/project/api/urls.py
@@ -1,6 +1,37 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 app_name = 'api'
 
 urlpatterns = [
-
+    # AUTH #################################
+    path(
+        route='token/',
+        view=TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        route='token/refresh/',
+        view=TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        route='token/verify/',
+        view=TokenVerifyView.as_view(),
+        name='token_verify'
+    ),
+    # path(
+    #     route='auth/password-reset/',
+    #     view=PasswordResetCodeView.as_view(),
+    #     name='password-reset-code'
+    # ),
+    # path(
+    #     route='auth/password-reset/validate/',
+    #     view=PasswordValidationView.as_view(),
+    #     name='password-reset_verify'
+    # ),
 ]
